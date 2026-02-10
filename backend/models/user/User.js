a5db83892e080cfa1115a11df0e6905ffb052543
Anton Abitharshan <aptharshan@gmail.com>
2026-02-11 01:16:22 +0530

    minor update on user model for work without pw for O Auth

--- backend/models/user/User.js
+++ backend/models/user/User.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
     // Basic Identity
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
+    password: { type: String, required: false },
     role: { type: String, enum: ["user", "admin", "mentor"], default: "user" },
 
     // Professional Profile
